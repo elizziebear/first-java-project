5b70f08e6583448fef5cd2e8745154f1fec73676
janeym <jane.elizabeth.mark@gmail.com>
2024-04-02 09:55:07 -0400

    initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        mainFrame popUP = new mainFrame();
+        popUP.init();
+    }
+}
